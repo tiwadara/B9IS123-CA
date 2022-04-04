d0517b0d80db2118d01232130f396d24a7a93ee7
Tiwa Babalola <tbabalola@getcarbon.co>
2022-04-04 21:56:50 +0100

    feat: added failing tests for compute usecase

--- src/employee.py
+++ src/employee.py
@@ -1,12 +1,17 @@
 # Create a class employee
 
 class Employee:
-    def __init__(self):
-        self.staff_id = None
-        self.first_name = None
-        self.last_name = None
-        self.reg_hours = None
-        self.hourly_rate = None
-        self.otm_multiple = None
-        self.tax_credit = None
-        self.standard_band = None
+    def __init__(self, staff_id=None, first_name=None, last_name=None, reg_hours=None, hourly_rate=None,
+                 otm_multiple=None, tax_credit=None, standard_band=None):
+        self.staff_id = staff_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.reg_hours = reg_hours
+        self.hourly_rate = hourly_rate
+        self.otm_multiple = otm_multiple
+        self.tax_credit = tax_credit
+        self.standard_band = standard_band
+
+    def compute_payment(self, hours_worked, date):
+        pass
+
--- tests/test_employee.py
+++ tests/test_employee.py
@@ -7,14 +7,49 @@ class TestEmployee(TestCase):
 
     def test_when_create_new_employee_all_attributes_are_default(self):
         new_employee = Employee()
+        self.assertEqual(None, new_employee.staff_id)
         self.assertEqual(None, new_employee.first_name)
         self.assertEqual(None, new_employee.last_name)
-        self.assertEqual(None, new_employee.staff_id)
         self.assertEqual(None, new_employee.hourly_rate)
         self.assertEqual(None, new_employee.reg_hours)
         self.assertEqual(None, new_employee.otm_multiple)
         self.assertEqual(None, new_employee.tax_credit)
         self.assertEqual(None, new_employee.standard_band)
 
-    def test_compute_net_pay(self):
-        self.fail()
+    def test_when_a_new_employee_is_initialized_then_the_exact_values_are_returned(self):
+        new_employee = Employee(12345, 'Green', 'Joe', 37, 16, 1.5, 72, 710)
+        self.assertEqual(12345, new_employee.staff_id)
+        self.assertEqual('Green', new_employee.first_name)
+        self.assertEqual('Joe', new_employee.last_name)
+        self.assertEqual(37, new_employee.reg_hours)
+        self.assertEqual(16, new_employee.hourly_rate)
+        self.assertEqual(1.5, new_employee.otm_multiple)
+        self.assertEqual(72, new_employee.tax_credit)
+        self.assertEqual(710, new_employee.standard_band)
+
+    def test_when_compute_payment_is_called_the_payment_dictionary_is_returned(self):
+        new_employee = Employee(12345, 'Green', 'Joe', 37, 16, 1.5, 72, 710)
+        hours_worked = 10
+        date = '31/10/2021'
+        response = {
+            "name": "Joe Green",
+            "Date": "31/10/2021",
+            "Regular Hours Worked": 37,
+            "Overtime Hours Worked": 5,
+            "Regular Rate": 16,
+            "Overtime Rate": 24,
+            "Regular Pay": 592,
+            "Overtime Pay": 120,
+            "Gross Pay": 712,
+            "Standard Rate Pay": 710,
+            "Higher Rate Pay": 2,
+            "Standard Tax": 142,
+            "Higher Tax": 0.8,
+            "Total Tax": 142.8,
+            "Tax Credit": 72,
+            "Net Tax": 70.8,
+            "PRSI": 28.48,
+            "Net Deductions": 99.28
+        }
+        payment = new_employee.compute_payment(hours_worked, date)
+        self.assertEqual(response, payment)
